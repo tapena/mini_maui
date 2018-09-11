7ce77762d305f6ca274f9bec8015652f826b4f9c
Tatiana Pena <Tatiana.c.pena1@gmail.com>
2018-09-10 20:28:18 -0500

    09.10.18/version3

--- app/controllers/api/kids_controller.rb
+++ app/controllers/api/kids_controller.rb
@@ -3,5 +3,20 @@ class Api::KidsController < ApplicationController
   def all_kids_action
     @kids = Kid.all
     render 'all_kids_view.json.jbuilder'
-  end   
+  end  
+
+  def first_kid_action
+    @kid = Kid.first
+    render 'first_kid.json.jbuilder'
+  end
+
+  def second_kid_action
+    @kid = Kid.find_by(id: 2)
+    render 'second_kid.json.jbuilder'
+  end 
+
+  def third_kid_action
+    @kid = Kid.find_by(id: 3)
+    render 'third_kid.json.jbuilder'
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_kids_url" => 'kids#all_kids_action'
+    get "/first_kid_url" => 'kids#first_kid_action'
+    get "/second_kid_url" => 'kids#second_kid_action'
+    get "/third_kid_url" => 'kids#third_kid_action'
   end
 end
